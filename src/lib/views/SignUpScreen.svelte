2a8d3483acf2f295ef5eecaec45c3f3ec5646be5
LeaveItToBeaver <jasonbeaverw99@gmail.com>
2023-06-23 16:22:06 -0400

    Added color explosion effect. It needs work.

--- src/lib/views/SignUpScreen.svelte
+++ src/lib/views/SignUpScreen.svelte
@@ -1,22 +1,27 @@
 <script>
-    import { supabase } from '$lib/supabaseClient'; 
-	import { Container } from 'postcss';
-  
-    let email = '';
-    let password = '';
-  
-    async function signUp() {
+  import { supabase } from '$lib/supabaseClient'; 
+  import Textbox from '$lib/components/userlogin/TextBox.svelte';
+  import Button from '$lib/components/userlogin/Button.svelte';
+
+  let email = '';
+  let password = '';
+
+  async function signUp() {
       const { data, error } = await supabase.auth.signUp({
-        email: email,
-        password: password
+          email: email,
+          password: password
       })
-    }
+  }
 </script>
-<div class="flex w-full flex-auto justify-center border-blue-400 border-2">
-    <div class="flex flex-col max-w-min justify-center items-center align-middle border-yellow-600 border-2 ">
-        <input class="border-red-600 border-2 w-auto max-w-lg m-2" bind:value={email} placeholder="Email" />
-        <input class="m-2 border-red-600 border-2" bind:value={password} type="password" placeholder="Password" />
-        <button class="rounded-full border-cyan-700 border-2 w-3/4" on:click={signUp}>Sign Up!</button>
-    </div>
+
+<div class="flex flex-auto justify-center items-center">
+  <h1>Welcome!</h1>
 </div>
 
+<div class="flex w-full h-screen flex-auto justify-center items-center">
+    <div class="flex flex-col max-w-min justify-center items-center align-middle border-yellow-600 border-2">
+        <Textbox bind:value={email} placeholder="Email" />
+        <Textbox bind:value={password} placeholder="Password" password={true} />
+        <Button label="Sign Up!" on:click={signUp} />
+    </div>
+</div>
