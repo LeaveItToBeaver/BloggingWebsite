090ead0d32f9814ec1ffe4cc3a06068cf0c11284
LeaveItToBeaver <jasonbeaverw99@gmail.com>
2023-06-28 17:50:03 -0400

    Added parameter based routing for user auth.

--- src/lib/views/LoginScreen.svelte
+++ src/lib/views/LoginScreen.svelte
@@ -1,66 +1,62 @@
 <script lang="ts">
-  import { supabase } from '$lib/supabaseClient'; 
-  import Textbox from '$lib/components/userlogin/AuthTextBox.svelte';
-  import Button from '$lib/components/userlogin/AuthButton.svelte';
-  import { goto } from '$app/navigation'
-  import { writable } from 'svelte/store';
+	import { supabase } from '$lib/supabaseClient';
+	import Textbox from '$lib/components/userlogin/AuthTextBox.svelte';
+	import Button from '$lib/components/userlogin/AuthButton.svelte';
+	import { goto } from '$app/navigation';
+	import { writable } from 'svelte/store';
 
-  let validationError = writable('');
-  let email:string = '';
-  let password:string = '';
-  let formSubmitted:boolean = false;
+	let validationError = writable('');
+	let email: string = '';
+	let password: string = '';
+	let formSubmitted: boolean = false;
 
-function isValidEmail(email: string) {
-    const regex = /^\S+@\S+\.\S+$/;
-    return regex.test(email);
-}
+	function isValidEmail(email: string) {
+		const regex = /^\S+@\S+\.\S+$/;
+		return regex.test(email);
+	}
 
-  async function signIn() {
+	async function signIn() {
+		formSubmitted = true;
 
-    formSubmitted = true;
+		if (!isValidEmail) {
+			validationError.set('Please enter a valid email.');
+			return;
+		}
 
-    if (!isValidEmail){
-      validationError.set('Please enter a valid email.');
-      return;
-    }
+		if (password.length < 6) {
+			validationError.set('Password must be at least 6 characters');
+			return;
+		}
 
-    if(password.length < 6){
-        validationError.set('Password must be at least 6 characters');
-        return;
-    }
+		if (!/[A-Z]/.test(password) || !/\d/.test(password)) {
+			validationError.set('Password must contain at least one uppercase letter and one number');
+			return;
+		}
 
-    if (!/[A-Z]/.test(password) || !/\d/.test(password)) {
-        validationError.set('Password must contain at least one uppercase letter and one number');
-        return;
-    }
-    
-    const { data, error } = await supabase.auth.signInWithPassword({
-      email: email,
-      password: Password
-    });
-    
-    console.log("User Created At:", data.session?.user.confirmed_at);
-    console.log("%cUser Creation Error: " + error?.cause, 'color: red');
-      
-    if (error) {
-      validationError.set(error.message);
-      return;
-    }
-      
-    formSubmitted = false;
-    validationError.set(''); 
+		const { data, error } = await supabase.auth.signInWithPassword({
+			email: email,
+			password: password
+		});
 
-    if (!error) {
-      formSubmitted = false;
-      validationError.set('');
-      if (data.user.confirmed_at) {
-        goto('/user/' + );
-      };
-    }
-  }
-}
+		console.log('User Created At:', data.session?.user.confirmed_at);
+		console.log('%cUser Creation Error: ' + error?.cause, 'color: red');
 
+		if (error) {
+			validationError.set(error.message);
+			return;
+		}
 
+		formSubmitted = false;
+		validationError.set('');
+
+		if (!error) {
+			formSubmitted = false;
+			validationError.set('');
+			if (data.user.confirmed_at) {
+				goto('/user/' + data.user.id);
+			}
+		}
+	}
 </script>
 
 <div class="flex w-full h-screen bg-transparent justify-center items-center text-white">
